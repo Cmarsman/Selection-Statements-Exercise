6a52c275a54fa4e4fb86f2fb833b837c9e81e9a7
Cmarsman <chris_marsden@yahoo.com>
2024-05-01 10:34:27 -0500

    made first exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number guessing Game
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+            Console.WriteLine("Pick a number from 1 - 10");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            { 
+            Console.WriteLine($"{userInput} is too low");
+            }
+            else if( userInput > favNumber) 
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
